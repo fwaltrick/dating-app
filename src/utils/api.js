097696469e53f248becbdbaa3b6c53e83038bdea
Fabricio Waltrick <fabricio.waltrick@gmail.com>
2019-10-29 13:24:29 +0100

    Getting started. Set up tools (ESLint, Prettier, Babel, Parcel). Add function in api.js to consume API.

---
+++ src/utils/api.js
@@ -0,0 +1,23 @@
+async function getUsers(sort = 'ACTIVITY') {
+  /// Fetching sorted users general information
+  const response = await fetch(
+    `http://localhost:3000/api/search?length=32&sorting=${sort}`
+  )
+  const json = await response.json()
+  const data = json.items
+
+  // Fetching users detailed profiles
+  const queryProfilesList = data.map(item => item.id).join('&ids=')
+  const profilesList = await fetch(
+    `http://localhost:3000/api//profiles?ids=${queryProfilesList}`
+  )
+  const detailedData = await profilesList.json()
+
+  // Merging general and detailed profiles to populate the cards
+  const mergedProfile = await data.map(function(item) {
+    const same = detailedData.find(el => el.id === item.id)
+    return { ...same, ...item }
+  })
+
+  return mergedProfile
+}
